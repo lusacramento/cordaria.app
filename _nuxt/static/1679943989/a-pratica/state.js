5b914572da6538aef12485141a1dfe78e2af22ef
Lu Sacramento <lucianossacramento@yahoo.com.br>
2023-03-27 16:15:23 -0300

    test3

--- _nuxt/static/1679943989/a-pratica/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1679943989",layout:"default",error:a,serverRendered:true,routePath:"\u002Fa-pratica",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c6f19395"},http:{browserBaseURL:b}}}}(null,"\u002F"));
